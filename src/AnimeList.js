0e2050f90f02eff4c8d31b8082ac9059cab81faf
Kevin <kevinloc@hotmail.com>
2026-03-17 00:16:40 -0300

    correcao

---
+++ src/AnimeList.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+function AnimeList() {
+  const [animes, setAnimes] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.jikan.moe/v4/top/anime')
+      .then((res) => res.json())
+      .then((data) => setAnimes(data.data))
+  }, [])
+
+  return (
+    <div>
+      {animes.map((anime) => (
+        <div key={anime.mal_id}>
+          <h2>{anime.title}</h2>
+          <img src={anime.images.jpg.image_url} width="200" />
+          <p>Ano: {anime.year}</p>
+          <p>Episódios: {anime.episodes}</p>
+          <p>Nota: {anime.score}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AnimeList
